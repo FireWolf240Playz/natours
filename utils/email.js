e6c916e0d92f36964603b3869297a3bf5a6a4c7e
Alexander Yordanov <a.yordanow67@gmail.com>
2024-11-16 13:14:56 +0200

    Send email functionality

--- utils/email.js
+++ utils/email.js
@@ -1,27 +1,71 @@
 const nodemailer = require("nodemailer");
+const pug = require("pug");
+const htmlToText = require("html-to-text");
 
-const sendEmail = async (options) => {
-  // 1) Create a transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(" ")[0];
+    this.url = url;
+    this.from = `Alexander Yordanov <${process.env.EMAIL_FROM}>`;
+  }
 
-  // 2) Define the email options
-  const mailOptions = {
-    from: "Jonas Schmedtmann <hello@jonas.io>",
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    // html:
-  };
+  newTransport() {
+    if (process.env.NODE_ENV === "production") {
+      // Brevo (Sendinblue) SMTP configuration
+      return nodemailer.createTransport({
+        service: "Sendinblue",
+        host: process.env.BREVO_SMTP_SERVER,
+        port: process.env.BREVO_PORT,
+        auth: {
+          user: process.env.BREVO_LOGIN,
+          pass: process.env.BREVO_API_KEY,
+        },
+      });
+    }
 
-  // 3) Actually send the email
-  await transporter.sendMail(mailOptions);
+    // Development configuration
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  async send(template, subject) {
+    //Send the actual email
+    //1) Render html based on a pug template
+
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject,
+    });
+
+    //2) Define ne emial options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.htmlToText(html),
+    };
+
+    //3) Create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send("welcome", "Welcome to the Natours Family!");
+  }
+
+  async sendResetPassword() {
+    await this.send(
+      "passwordReset",
+      "Your password reset link (valid for only 10 minutes)",
+    );
+  }
 };
-
-module.exports = sendEmail;
